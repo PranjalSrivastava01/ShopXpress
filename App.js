7853555606ee2ccf6e8e814cac22b5b41df50fc8
PRANJAL <pranjals0105@gmail.com>
2023-06-16 23:38:35 +0530

    second commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {MyTabs} from './navigation/BottomNavigation';
+import {OnboardingScreenNavigation} from './navigation/BottomNavigation';
+import { Provider } from 'react-redux';
+import store from './screens/Redux/store/Store';
+const App = () => {
+  return (
+    <>
+    <Provider store={store}>
+    <NavigationContainer>
+      <OnboardingScreenNavigation />
+        {/* <MyTabs /> */}
+      </NavigationContainer>
+    </Provider>
+    </>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
